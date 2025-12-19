5a3d3b21026dd71cd2a409cf696fe804479f991a
giahuy <boomhuyxt@gmail.com>
2025-12-19 14:06:56 +0700

    Add project files.

---
+++ Forms/FormXuat.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace App_QL_kho
+{
+    partial class FormXuat
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormXuat));
+            this.pnl_productInfo = new System.Windows.Forms.Panel();
+            this.cb_camera = new System.Windows.Forms.ComboBox();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.txt_box = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pnl_productInfo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnl_productInfo
+            // 
+            this.pnl_productInfo.Controls.Add(this.cb_camera);
+            this.pnl_productInfo.Controls.Add(this.btnStop);
+            this.pnl_productInfo.Controls.Add(this.txt_box);
+            this.pnl_productInfo.Controls.Add(this.btnStart);
+            this.pnl_productInfo.Controls.Add(this.pictureBox);
+            this.pnl_productInfo.Controls.Add(this.label1);
+            this.pnl_productInfo.Location = new System.Drawing.Point(734, 11);
+            this.pnl_productInfo.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pnl_productInfo.Name = "pnl_productInfo";
+            this.pnl_productInfo.Size = new System.Drawing.Size(401, 555);
+            this.pnl_productInfo.TabIndex = 1;
+            // 
+            // cb_camera
+            // 
+            this.cb_camera.FormattingEnabled = true;
+            this.cb_camera.Location = new System.Drawing.Point(56, 46);
+            this.cb_camera.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cb_camera.Name = "cb_camera";
+            this.cb_camera.Size = new System.Drawing.Size(298, 24);
+            this.cb_camera.TabIndex = 4;
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(253, 296);
+            this.btnStop.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(101, 27);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "Tắt";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // txt_box
+            // 
+            this.txt_box.Location = new System.Drawing.Point(22, 342);
+            this.txt_box.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txt_box.Multiline = true;
+            this.txt_box.Name = "txt_box";
+            this.txt_box.Size = new System.Drawing.Size(364, 191);
+            this.txt_box.TabIndex = 1;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(56, 296);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(101, 27);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Bật";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(56, 74);
+            this.pictureBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(298, 177);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox.TabIndex = 2;
+            this.pictureBox.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(101, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(210, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Thông tin sản phẩm";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dataGridView1.Location = new System.Drawing.Point(25, 39);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(427, 527);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Column2";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Column3";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Column4";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(469, 57);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(245, 253);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // FormXuat
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(26)))), ((int)(((byte)(30)))));
+            this.ClientSize = new System.Drawing.Size(1161, 602);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.pnl_productInfo);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "FormXuat";
+            this.Text = "PhieuXuat";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.pnl_productInfo.ResumeLayout(false);
+            this.pnl_productInfo.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel pnl_productInfo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cb_camera;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.TextBox txt_box;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Forms/FormXuat.cs
@@ -0,0 +1,43 @@
+﻿using App_QL_kho.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace App_QL_kho
+{
+    public partial class FormXuat : Form
+    {
+        private CameraQR camera;
+        public FormXuat()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            camera = new CameraQR(cb_camera, pictureBox, txt_box);
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            camera.Stop();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            camera.Start();
+
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            camera.Stop();
+        }
+    }
+}
